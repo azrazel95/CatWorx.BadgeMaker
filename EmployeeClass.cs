508f037cb349bd0d15343d7be00a8716a905901d
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-06-22 13:13:24 -0400

    added a class

---
+++ EmployeeClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatWorx.BadgeMaker
+{
+    class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public int Id;
+        public string PhotoUrl;
+        public Employee(string firstName, string lastName) {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,25 +4,34 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        static List<string> GetEmployees()
         {
-           List<string> employees = new List<string>();
+            List<string> employees = new List<string>();
             while (true)
             {
-                Console.WriteLine("Please enter a name: (leave empty to exit)");
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
                 string input = Console.ReadLine() ?? "";
-                // break if the user hits enter without typing a name
-                if (input == "" )
-                {
-                    break;
-                }
-                employees.Add(input);
-            }
+                if (input == "")
+                { break; }
+                Employee currentEmployee = new Employee(input, "Smith");
+                employees.Add(currentEmployee.GetFullName());
 
-            for (int i = 0; i<employees.Count; i++)
+            }
+            return employees;
+        }
+
+        static void PrintEmployees(List<string> employees)
+        {
+            for (int i = 0; i< employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
             }
         }
+        static void Main(string[] args) {
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
+        }
+
     }
 }
