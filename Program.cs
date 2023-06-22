ff5371df88aac679e68906a93c49ac46694b3232
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-06-22 10:04:46 -0400

    added funny calculations

--- Program.cs
+++ Program.cs
@@ -15,6 +15,19 @@ namespace CatWorx.BadgeMaker
             double area = side * side;
             Console.WriteLine(area);
             Console.WriteLine("area is a {0}", area.GetType().Name);
+            Console.WriteLine("12" + "3"); //should spit out 123
+            Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
+            Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
+            Console.WriteLine(1 + 2 * 3);     // order of operations
+            Console.WriteLine(10 / 3.0);      // int's and doubles
+            Console.WriteLine(10 / 3);        // int's 
+            
+
+            int num = 10;
+            num += 100;
+            Console.WriteLine(num);
+            num++;
+            Console.WriteLine(num);
         }
     }
 }
