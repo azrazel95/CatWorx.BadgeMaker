e32d0af5508d6d7dc19ed8b371d78f45cf99b700
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-06-22 22:48:14 -0400

    employee data being stored

--- Program.cs
+++ Program.cs
@@ -5,31 +5,36 @@ namespace CatWorx.BadgeMaker
     class Program
     {
         
-        static List<string> GetEmployees()
+        static List<Employee> GetEmployees()
         {
-            List<string> employees = new List<string>();
+            List<Employee> employees = new List<Employee>();
             while (true)
             {
                 Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine() ?? "";
-                if (input == "")
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
                 { break; }
-                Employee currentEmployee = new Employee(input, "Smith");
-                employees.Add(currentEmployee.GetFullName());
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? " ";
+                Console.Write("Enter Id: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                string photoUrl = Console.ReadLine() ?? "";
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
 
             }
             return employees;
         }
 
-        static void PrintEmployees(List<string> employees)
+        static void PrintEmployees(List<Employee> employees)
         {
             for (int i = 0; i< employees.Count; i++)
             {
-                Console.WriteLine(employees[i]);
+                Console.WriteLine(employees[i].GetFullName());
             }
         }
         static void Main(string[] args) {
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
             PrintEmployees(employees);
         }
 
