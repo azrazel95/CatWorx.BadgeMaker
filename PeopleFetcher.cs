f6106d44f9f3be6680a7114286f6f476045306ee
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-06-24 17:44:33 -0400

    accessed a json object with a subquery

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -17,9 +17,12 @@ namespace CatWorx.BadgeMaker
             {
                 string response = await client.GetStringAsync("https://randomuser.me/api/?results=10&nat=us&inc=name,id,picture");
                 JObject json = JObject.Parse(response);
-                Console.WriteLine(json.SelectToken("results[0].name.first"));
-                Console.WriteLine(json.SelectToken("results[1].name.first"));
-                Console.WriteLine(json.SelectToken("results[2].name.first"));
+                JArray results = (JArray)json["results"];
+                foreach (JToken person in results)
+                {
+                    Console.WriteLine(person["name"]["first"]);
+                }
+                
             }
             return employees;
           
