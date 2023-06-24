e77ec2b02f58c44caac2f9adbb2ef0613ef2d9fa
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-06-23 22:14:17 -0400

    added utils

---
+++ Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatWorx.BadgeMaker
+{
+    class Util
+    {
+        public static void PrintEmployees(List<Employee> employees)
+        {
+            
+        for (int i = 0; i < employees.Count; i++)
+           {
+              string template = "{0,-10}\t{1,-20}\t{2}";
+              Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+        }
+        }
+    }
+}
